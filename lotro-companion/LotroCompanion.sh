c822271af004164722b0c0966afa7dae148a4af9
Damien Morcellet <dmorcellet@free.fr>
2013-02-19 07:39:40 +0000

    delta4j/lotro-distrib:     * fixed version 2.0

--- lotro-companion/LotroCompanion.sh
+++ lotro-companion/LotroCompanion.sh
@@ -3,13 +3,14 @@
 JAVA_OPTS="-Xms8M -Xmx32M -Dcom.sun.management.jmxremote -Duser.language=en"
 
 # Setup boot classpath
-CLASSPATH=lib/commons-codec-1.2.jar
+CLASSPATH=lib/patchs/log-config.jar
+CLASSPATH=${CLASSPATH}:lib/commons-codec-1.2.jar
 CLASSPATH=${CLASSPATH}:lib/commons-httpclient-3.1.jar
 CLASSPATH=${CLASSPATH}:lib/commons-logging-1.0.4.jar
-CLASSPATH=${CLASSPATH}:lib/delta-common-1.1-SNAPSHOT.jar
-CLASSPATH=${CLASSPATH}:lib/delta-downloads-1.1-SNAPSHOT.jar
+CLASSPATH=${CLASSPATH}:lib/delta-lotro-2.0.jar
 CLASSPATH=${CLASSPATH}:lib/delta-json-1.0.jar
-CLASSPATH=${CLASSPATH}:lib/delta-lotro-1.1-SNAPSHOT.jar
+CLASSPATH=${CLASSPATH}:lib/delta-downloads-1.1.jar
+CLASSPATH=${CLASSPATH}:lib/delta-common-1.1.jar
 CLASSPATH=${CLASSPATH}:lib/jcommon-1.0.16.jar
 CLASSPATH=${CLASSPATH}:lib/jericho-html-3.2.jar
 CLASSPATH=${CLASSPATH}:lib/jfreechart-1.0.13.jar
