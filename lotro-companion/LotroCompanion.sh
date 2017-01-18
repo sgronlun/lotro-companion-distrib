32946e739f3d84fe58e112c309291d7978fa77e5
Damien Morcellet <dmorcellet@free.fr>
2017-01-18 18:06:28 +0000

    delta4j/lotro-distrib:     * Updates for the future release 4.0     * Updated the items database for 19.2.2     * Added the relics database     * Removed unused data (quests.zip, deeds.zip)     * Removed unused jars (downloads, jericho, commons*)

--- lotro-companion/LotroCompanion.sh
+++ lotro-companion/LotroCompanion.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-JAVA_OPTS="-Xms8M -Xmx32M -Dcom.sun.management.jmxremote -Duser.language=en"
+JAVA_OPTS="-Xms8M -Xmx512M -Dcom.sun.management.jmxremote -Duser.language=en"
 
 # Setup boot classpath
 CLASSPATH=lib/patchs/log-config.jar
-CLASSPATH=${CLASSPATH}:lib/commons-codec-1.2.jar
-CLASSPATH=${CLASSPATH}:lib/commons-httpclient-3.1.jar
-CLASSPATH=${CLASSPATH}:lib/commons-logging-1.0.4.jar
-CLASSPATH=${CLASSPATH}:lib/delta-lotro-3.0.jar
-CLASSPATH=${CLASSPATH}:lib/delta-json-1.0.jar
-CLASSPATH=${CLASSPATH}:lib/delta-downloads-1.1.jar
-CLASSPATH=${CLASSPATH}:lib/delta-common-1.1.jar
+CLASSPATH=${CLASSPATH}:lib/delta-common-1.2-SNAPSHOT.jar
+CLASSPATH=${CLASSPATH}:lib/delta-lotro-core-4.0-SNAPSHOT.jar
+CLASSPATH=${CLASSPATH}:lib/delta-lotro-4.0-SNAPSHOT.jar
 CLASSPATH=${CLASSPATH}:lib/jcommon-1.0.16.jar
-CLASSPATH=${CLASSPATH}:lib/jericho-html-3.2.jar
 CLASSPATH=${CLASSPATH}:lib/jfreechart-1.0.13.jar
 CLASSPATH=${CLASSPATH}:lib/log4j-1.2.14.jar
+CLASSPATH=${CLASSPATH}:lib/icons.zip
+CLASSPATH=${CLASSPATH}:lib/relicIcons.zip
 
 # Execute the Java Virtual Machine
 exec java ${JAVA_OPTS} -classpath "${CLASSPATH}" delta.games.lotro.Main "$@"
