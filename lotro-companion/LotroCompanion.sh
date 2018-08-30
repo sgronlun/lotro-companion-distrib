c0f98d7ec940dec3d3daa74c3b845c7410080060
Damien Morcellet <dmorcellet@free.fr>
2018-08-30 18:57:48 +0200

    Integrated version 9.0

--- lotro-companion/LotroCompanion.sh
+++ lotro-companion/LotroCompanion.sh
@@ -2,17 +2,18 @@
 
 JAVA_OPTS="-Xms256M -Xmx512M -Dcom.sun.management.jmxremote -Duser.language=en"
 
+P=`pwd`
 # Setup boot classpath
-CLASSPATH=lib/delta-lotro-companion-8.0.jar
-CLASSPATH=${CLASSPATH}:lib/delta-lotro-maps-1.2.jar
-CLASSPATH=${CLASSPATH}:lib/delta-lotro-core-8.0.jar
-CLASSPATH=${CLASSPATH}:lib/delta-common-ui-1.7.jar
-CLASSPATH=${CLASSPATH}:lib/delta-common-1.4.jar
-CLASSPATH=${CLASSPATH}:lib/jcommon-1.0.16.jar
-CLASSPATH=${CLASSPATH}:lib/jfreechart-1.0.13.jar
-CLASSPATH=${CLASSPATH}:lib/log4j-1.2.14.jar
-CLASSPATH=${CLASSPATH}:lib/icons.zip
-CLASSPATH=${CLASSPATH}:lib/relicIcons.zip
+CLASSPATH=${P}/lib/delta-lotro-companion-9.0.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/delta-lotro-maps-2.0.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/delta-lotro-core-9.0.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/delta-common-ui-1.8.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/delta-common-1.5.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/jcommon-1.0.16.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/jfreechart-1.0.13.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/log4j-1.2.14.jar
+CLASSPATH=${CLASSPATH}:${P}/lib/icons.zip
+CLASSPATH=${CLASSPATH}:${P}/lib/relicIcons.zip
 
 # Execute the Java Virtual Machine
 exec java ${JAVA_OPTS} -classpath "${CLASSPATH}" delta.games.lotro.Main "$@"
