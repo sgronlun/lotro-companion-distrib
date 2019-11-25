50d7800be7de3bc2e149f2df3a35d31600107d9e
Damien Morcellet <dmorcellet@free.fr>
2019-11-25 21:30:01 +0100

    Integrated version 11.0.25.0.2 (continued)

--- go.bat
+++ go.bat
@@ -1,7 +1,8 @@
 set ANT=D:\dam\lc\integration\apache-ant-1.10.1
 set INNO_HOME=C:\Program Files (x86)\Inno Setup 5
 set PATH=%ANT%\bin;%INNO_HOME%;%PATH%
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_141
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_192
+set JAVA_HOME=C:\Users\dm\Downloads\zulu8.38.0.13-ca-fx-jdk8.0.212-win_i686
 
-ant
+apache-ant-1.10.1\bin\ant
 
