b40516c7b449fdc3a4faa124470cfb4217fb14a0
Damien Morcellet <dmorcellet@free.fr>
2020-07-12 22:19:08 +0200

    Improved the integration script

--- go.bat
+++ go.bat
@@ -1,8 +1,6 @@
-set ANT=D:\dam\lc\integration\apache-ant-1.10.1
 set INNO_HOME=C:\Program Files (x86)\Inno Setup 5
-set PATH=%ANT%\bin;%INNO_HOME%;%PATH%
+set PATH=%INNO_HOME%;%PATH%
 rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_192
-set JAVA_HOME=C:\Users\dm\Downloads\zulu8.38.0.13-ca-fx-jdk8.0.212-win_i686
+set JAVA_HOME=C:\dam\softs\java\zulu8.38.0.13-ca-fx-jdk8.0.212-win_i686
 
 apache-ant-1.10.1\bin\ant
-
